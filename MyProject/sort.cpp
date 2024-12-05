4a740c92cfa3f992a0ed5fadeb227fe46d216d6e
Miroslav262 <mirostavchik235@gmail.com>
2024-12-05 19:21:26 +0300

    Объединение сортировок в 1 функцию, добавление функции вывод структуры в консоль

--- MyProject/sort.cpp
+++ MyProject/sort.cpp
@@ -81,7 +81,93 @@ void heapsort(info**arr, int size, long long(*criteriy)(info*data))
     swap_info(arr, 0, i);
   }
 }
-
+info** sorting(info** array, int size, bool method, bool kreteriy, bool filter_criteria, int & result_size)
+{
+    info** filtered;
+    if (!filter_criteria)
+    {
+        filtered = filter(array, size, check_by_mobile, result_size); // фильтрация по тарифу "мобильный"
+    }
+    else 
+    {
+        filtered = filter(array, size, check_by_date, result_size); // фильтрация по ноябрю 21 года
+    }
+		if(!kreteriy)
+		{
+			if(method)
+			{
+        qsort(filtered, 0, result_size - 1, make_seconds);//quick, по продолжительности
+			}
+			else
+			{
+        heapsort(filtered, result_size, make_seconds);//heap, по продолжительности
+			}
+		}
+		else 
+		{
+			if(method)
+			{
+        qsort(filtered, 0, result_size - 1, make_number);//quick, по номеру
+			}
+			else
+			{
+        heapsort(filtered, result_size, make_number);//heap, по номеру
+			}
+		}
+    return filtered;
+}
+void cout_info(info** information, int size, bool inverse)
+{
+    if (inverse) {
+        for (int i = 0; i < size; i++)
+        {
+            /********** вывод номера **********/
+            std::cout << "Номер: ";
+            std::cout << information[i]->number << '\n';
+            /********** дата звонка **********/
+            std::cout << "Дата звонка: ";
+            std::cout << information[i]->date.day << "." << information[i]->date.month << "." << information[i]->date.year << '\n';
+            /********** время звонка **********/
+            std::cout << "Время звонка: ";
+            std::cout << information[i]->time.hours << ":" << information[i]->time.minutes << ":" << information[i]->time.seconds << '\n';
+            /********** продолжительность звонка **********/
+            std::cout << "Продолжительность звонка: ";
+            std::cout << information[i]->lasting.hours << ":" << information[i]->lasting.minutes << ":" << information[i]->lasting.seconds << '\n';
+            /********** тариф **********/
+            std::cout << "Тариф: ";
+            std::cout << information[i]->rate << '\n';
+            /********** стоимость минуты разговвора **********/
+            std::cout << "Стоимость минуты разговвора: ";
+            std::cout << information[i]->cost << '\n';
+            std::cout << '\n';
+        }
+    }
+    else
+    {
+        for (int i = size - 1; i >= 0; i--)
+        {
+            /********** вывод номера **********/
+            std::cout << "Номер: ";
+            std::cout << information[i]->number << '\n';
+            /********** дата звонка **********/
+            std::cout << "Дата звонка: ";
+            std::cout << information[i]->date.day << "." << information[i]->date.month << "." << information[i]->date.year << '\n';
+            /********** время звонка **********/
+            std::cout << "Время звонка: ";
+            std::cout << information[i]->time.hours << ":" << information[i]->time.minutes << ":" << information[i]->time.seconds << '\n';
+            /********** продолжительность звонка **********/
+            std::cout << "Продолжительность звонка: ";
+            std::cout << information[i]->lasting.hours << ":" << information[i]->lasting.minutes << ":" << information[i]->lasting.seconds << '\n';
+            /********** тариф **********/
+            std::cout << "Тариф: ";
+            std::cout << information[i]->rate << '\n';
+            /********** стоимость минуты разговвора **********/
+            std::cout << "Стоимость минуты разговвора: ";
+            std::cout << information[i]->cost << '\n';
+            std::cout << '\n';
+        }
+    }
+}
 /*
 1)Вывести все телефонные разговоры на мобильные телефоны.
 2)Вывести все телефонные разговоры в ноябре 2021 года.
