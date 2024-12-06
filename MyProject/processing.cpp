140015b38ae090e7f1c018b6d26868ab2f9bae02
Miroslav262 <mirostavchik235@gmail.com>
2024-12-06 10:27:33 +0300

    Реализована функция process

--- MyProject/processing.cpp
+++ MyProject/processing.cpp
@@ -1,6 +1,13 @@
 #include "processing.h"
-
-int process(info* array[], int size)
+#include "sort.h"
+int process(info** array, int size)
 {
-	return 0;
+	double sum_count = 0, sum_time = 0;
+	for (int i = 0; i<size; i++) 
+	{
+		sum_count += make_seconds(array[i]) * array[i]->cost;
+		sum_time += make_seconds(array[i]);
+	}
+	return sum_count / sum_time;
+
 }
